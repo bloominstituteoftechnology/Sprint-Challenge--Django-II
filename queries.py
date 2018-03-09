fb7a859b52f35474197987f9d86855ad1f1edbaa
Samuel Kim <SamuelKim@Owners-MacBook-Air.local>
2018-03-09 14:11:08 -0800

    cleaned up code, still working on q6

--- queries.py
+++ queries.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from .models import Character, Fighter, Mage, Cleric, Thief
 from armory.models import Item, Weapon
+characterCount = Character.objects.all().count()
 def countCharacters(request):
     # returns 302 characters from the database using count method
-    return HttpResponse(Character.objects.all().count())
+    return HttpResponse(characterCount);
 def countSubCharacters(request):
     mageCount = Mage.objects.all().count()
     fighterCount = Fighter.objects.all().count()
@@ -23,10 +24,8 @@ def weaponCount(request):
     allcount = 'weapon count = %d; not weapon count = %d' %(countWeapon, notWeapon)
     return HttpResponse(allcount)
 def averageItemCount(request):
-    characterCount = Character.objects.all().count()
     inventoryCount = Character.objects.values('inventory').count()
     # return and rounds out to 3 items for each character
     return HttpResponse(round(inventoryCount/characterCount))
 def averageWeaponCount(reqest):
-    characterCount = Character.objects.all().count()
     weaponCount = Character.objects.values('')
