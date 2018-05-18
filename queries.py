675b7b6a2224ba5e6a2eddde1640419b3e845410
Julian Kohlman <juliankohlman@gmail.com>
2018-05-18 08:30:25 -0400

    Finish last django shell query

---
+++ queries.py
@@ -0,0 +1,22 @@
+# ? How many total Characters are there? ==> 302
+# Character.objects.count() 
+# ? How many of each specific subclass?
+''' 
+example: Fighter.objects.all().count()
+Fighter: 68
+Mage: 108
+Cleric: 75
+Thief: 51
+Necromancer: 11
+'''
+# ? How many total Items? ==> 174
+# Item.objects.all().count()
+
+# ? How many of the Items are weapons? How many are not? ==> 37 weapons : 137 non-weapon
+# Weapon.objects.all().count()
+# Item.objects.all().count() - Weapon.objects.all().count()
+
+# ? On average, how many Items does each Character have? ==> 89
+# Character.objects.aggregate(Avg('inventory'))
+
+# ? On average, how many Weapons does each character have?
