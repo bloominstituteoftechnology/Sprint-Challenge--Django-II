8bc343040cdd9ecd23d74721772b02733bf72e36
Samuel Cha <sunchipssam@gmail.com>
2018-05-18 13:40:29 -0700

    Added per sol lect

--- queries.py
+++ queries.py
@@ -2,7 +2,7 @@
 
 # - How many total Characters are there? ***************************************
 from charactercreator.models import Character  #                               *
-Character.objects.all().count()  # 302                                         *
+Character.objects.count()  # 302                                               *
 # ******************************************************************************
 
 # - How many of each specific subclass? ****************************************
@@ -12,95 +12,64 @@ import Mage  #                                                                 *
 import Cleric  #                                                               *
 import Thief  #                                                                *
 import Necromancer  #                                                          *
-Fighter.objects.all().count()  # 68                                            *
-Mage.objects.all().count()  # 108                                              *
-Cleric.objects.all().count()  # 75                                             *
-Thief.objects.all().count()  # 51                                              *
-Necromancer.objects.all().count()  # 11                                        *
+Fighter.objects.count()  # 68                                                  *
+Mage.objects.count()  # 97                                                     *
+Cleric.objects.count()  # 75                                                   *
+Thief.objects.count()  # 51                                                    *
+Necromancer.objects.count()  # 11                                              *
 #                                                                              *
-Fighter.objects.all().count() +  #                                             *
-Mage.objects.all().count() +  #                                                *
-Cleric.objects.all().count() +  #                                              *
-Thief.objects.all().count() +  #                                               *
-Necromancer.objects.all().count()  # 313                                       *
-#                                                                              *
-# Note: that because the `Necromancer` class is a subclass of `Mage`,          *
-# it does not get counted by `Character.objects.all()`.                        *
-# 302 + 11 = 313                                                               *
+Fighter.objects.count() +  #                                                   *
+Mage.objects.filter(necromancer__isnull=True).count() +  #                     *
+Cleric.objects.count() +  #                                                    *
+Thief.objects.count() +  #                                                     *
+Necromancer.objects.count()  # 302                                             *
 # ******************************************************************************
 
 # - How many total Items? ******************************************************
 from armory.models import Item  #                                              *
-Item.objects.all().count()  # 174                                              *
+Item.objects.count()  # 174                                                    *
 # ******************************************************************************
 
 # - How many of the Items are weapons? How many are not? ***********************
+from armory.models import Weapon  #                                            *
 Item.objects.filter(weapon__isnull=True).count()  # 137                        *
 Item.objects.filter(weapon__isnull=False).count()  # 37                        *
 # ******************************************************************************
 
 # - On average, how many Items does each Character have? ***********************
-(  #                                                                           *
-  Item.objects.filter(  #                                                      *
-    character__in=(x for x in Character.objects.all())).count() +  #           *
-  Item.objects.filter(  #                                                      *
-    character__in=(x for x in Necromancer.objects.all())).count()  #           *
-)  #                                                                           *
-/  #                                                                           *
-(  #                                                                           *
-  Character.objects.all().count() +  #                                         *
-  Necromancer.objects.all().count()  #                                         *
-)  # 2.9680511182108624                                                        *
-#                                                                              *
-# Not accounting for `Necromancer`s yields similar results (0.005 diff):       *
 Item.objects.filter(  #                                                        *
   character__in=(x for x in Character.objects.all())).count()  #               *
 /  #                                                                           *
-Character.objects.all().count()  # 2.9735099337748343                          *
+Character.objects.count()  # 2.9735099337748343                                *
 # ******************************************************************************
 
 # - On average, how many Weapons does each character have? *********************
-
-(  #                                                                           *
-  Weapon.objects.filter(  #                                                    *
-    character__in=(x for x in Character.objects.all())).count() +  #           *
-  Weapon.objects.filter(  #                                                    *
-    character__in=(x for x in Necromancer.objects.all())).count()  #           *
-)  #                                                                           *
-/  #                                                                           *
-(  #                                                                           *
-  Character.objects.all().count() +  #                                         *
-  Necromancer.objects.all().count()  #                                         *
-)  # 0.6741214057507987                                                        *
-#                                                                              *
-# Again, not accounting for `Necromancer`s yields very similar results:        *
 Weapon.objects.filter(  #                                                      *
   character__in=(x for x in Character.objects.all())).count()  #               *
 /  #                                                                           *
-Character.objects.all().count()  # 0.6721854304635762                          *
+Character.objects.count()  # 0.6721854304635762                                *
 # ******************************************************************************
 
 
 # unformatted queries
 from charactercreator.models import Character
-Character.objects.all().count() # 302
+Character.objects.count() # 302
 
 from charactercreator.models import Fighter, Mage, Cleric, Thief, Necromancer
-Fighter.objects.all().count() # 68
-Mage.objects.all().count()  # 108
-Cleric.objects.all().count()  # 75
-Thief.objects.all().count()  # 51
-Necromancer.objects.all().count()  # 11
-Fighter.objects.all().count() + Mage.objects.all().count() + Cleric.objects.all().count() + Thief.objects.all().count() + Necromancer.objects.all().count()  # 313
+Fighter.objects.count() # 68
+Mage.objects.count()  # 97
+Cleric.objects.count()  # 75
+Thief.objects.count()  # 51
+Necromancer.objects.count()  # 11
+Fighter.objects.count() + Mage.objects.filter(necromancer__isnull=True).count() + Cleric.objects.count() + Thief.objects.count() + Necromancer.objects.count()  # 302
 
 from armory.models import Item
-Item.objects.all().count()  # 174
+Item.objects.count()  # 174
 
+from armory.models import Weapon
 Item.objects.filter(weapon__isnull=True).count()  # 137
 Item.objects.filter(weapon__isnull=False).count()  # 37
 
-(Item.objects.filter(character__in=(x for x in Character.objects.all())).count() + Item.objects.filter(character__in=(x for x in Necromancer.objects.all())).count()) / (Character.objects.all().count() + Necromancer.objects.all().count())  # 2.9680511182108624
-Item.objects.filter(character__in=(x for x in Character.objects.all())).count() / Character.objects.all().count()  # 2.9735099337748343
+Item.objects.filter(character__in=(x for x in Character.objects.all())).count() / Character.objects.count()  # 2.9735099337748343
 
-(Weapon.objects.filter(character__in=(x for x in Character.objects.all())).count() + Weapon.objects.filter(character__in=(x for x in Necromancer.objects.all())).count()) / (Character.objects.all().count() + Necromancer.objects.all().count())  # 0.6741214057507987
-Weapon.objects.filter(character__in=(x for x in Character.objects.all())).count() / Character.objects.all().count()  # 0.6721854304635762
+Weapon.objects.filter(character__in=(x for x in Character.objects.all())).count() / Character.objects.count()  # 0.6721854304635762
