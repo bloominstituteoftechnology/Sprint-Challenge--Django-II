b48ea1f619fc56533e8d1a906ee6544677af2535
KickMe <kickme@alifs-mbp.gateway.sonic.net>
2018-05-18 19:09:40 -0700

    character count, adding rest soon

---
+++ queries.py
@@ -0,0 +1,8 @@
+
+# To find out the total Characters, I used dir() and utilized the count() method - Character.objects.count(). 
+# You would use the class name in place of Character. 
+# When I used this I found that there were 302 characters
+
+
+# Was having an issue with my computer so i was using my phone for the afternoon and now it's working well, so i will submit the other questions this evening or tomorrow. Just going to commit this for now. 
+
