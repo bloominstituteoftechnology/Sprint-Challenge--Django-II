55aa246304f344ebfef0d07ea0b4df0f4d07a503
Tyson Williams <tyson.j.williams@gmail.com>
2018-06-01 12:38:51 -0600

    Query counts for each subclass of Characters

--- queries.py
+++ queries.py
@@ -2,10 +2,14 @@
 
 # How many total Characters are there?
 from charactercreator.models import Character
-len(Character.objects.all()) # 302
+len(Character.objects.all())
 
 # How many of each specific subclass?
-
+len(Character.objects.filter(fighter=True))
+len(Character.objects.filter(mage=True))
+len(Character.objects.filter(cleric=True))
+len(Character.objects.filter(thief=True))
+# need query count for necromancer
 
 # How many total Items?
 
