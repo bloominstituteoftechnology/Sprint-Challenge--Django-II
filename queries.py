ee5708bd79c484e44c3f39d3c1e74c5450d77196
Charlie Sparks <csparks@antioch.edu>
2018-06-01 14:42:48 -0700

    finished all main goals

--- queries.py
+++ queries.py
@@ -20,3 +20,22 @@ print("The total of theif characters: " + str(theifChars))
 
 necroChars = len(Necromancer.objects.all())
 print("The total of Necromancer characters: " + str(necroChars))
+
+
+# total number of items
+totalItems = len(Item.objects.all())
+print("The total number of items is: " + str(totalItems))
+
+totalWeapons = len(Weapon.objects.all())
+print("The total number of weapons is: " + str(totalWeapons))
+
+totalOtherItems = totalItems - totalWeapons
+print("The total number of items that are not weapons is: " + str(totalOtherItems))
+
+# average number of items per character
+averageItems = (totalItems / totalChars)
+print("The average number of items for each character is: " + str(averageItems))
+
+# average number of weapons per character
+averageWeapons = (totalWeapons / totalChars)
+print("The average number of weapons for each character is: " + str(averageWeapons))
