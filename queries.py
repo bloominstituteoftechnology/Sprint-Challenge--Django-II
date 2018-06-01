c54243a2addffd478858901876c97a64f1ed705d
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-06-01 13:17:18 -0500

    Answer Two
    - in queries.py:
    - query each of the character classes
    - print results
    - comment the results from the cli

--- queries.py
+++ queries.py
@@ -1,5 +1,6 @@
-from charactercreator.models import Character
+from charactercreator.models import Character, Fighter, Mage, Cleric, Thief, Necromancer
 from armory.models import Item, Weapon
+
 ## How many total Characters are there?
 characters = Character.objects.all()
 char_result = len(characters)
@@ -8,7 +9,20 @@ print("There are", char_result, "characters in total")
 """ There are 302 total characters """
 
 ## How many of each specific subclass?
+print("There are", len(Fighter.objects.all()), "fighters in total")
+print("There are", len(Mage.objects.all()), "mages in total")
+print("There are", len(Cleric.objects.all()), "clerics in total")
+print("There are", len(Thief.objects.all()), "thieves in total")
+print("There are", len(Necromancer.objects.all()), "necromancers in total")
 
+"""
+68 fighters
+108 mages
+75 clerics
+51 thieves
+11 necromancers
+*** note that the total here is 313, because necromancers are mages, which have also been counted ***
+"""
 
 ## How many total Items?
 items = Item.objects.all()
