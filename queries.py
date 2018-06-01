1d98a79e770cc86a6d79b44254da4aed58ff5529
JohnJohnX4 <johnjohnx4@gmail.com>
2018-06-01 12:19:02 -0400

    project start

---
+++ queries.py
@@ -0,0 +1,6 @@
+#How many total Characters are there?
+#How many of each specific subclass?
+#How many total Items?
+#How many of the Items are weapons? How many are not?
+#On average, how many Items does each Character have?
+#On average, how many Weapons does each character have?
