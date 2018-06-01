f2eda1cee029f65b200c69d7798e857715266ee3
Tyson Williams <tyson.j.williams@gmail.com>
2018-06-01 11:41:29 -0600

    Add queries.py file for answering goal questions

---
+++ queries.py
@@ -0,0 +1,18 @@
+### Write Python code that uses the Django ORM to answer:
+
+# How many total Characters are there?
+
+
+# How many of each specific subclass?
+
+
+# How many total Items?
+
+
+# How many of the Items are weapons? How many are not?
+
+
+# On average, how many Items does each Character have?
+
+
+# On average, how many Weapons does each character have?
