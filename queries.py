07f9c82c91a83b23c06a778c99fb7bf78fe72783
charletta <charlettabullard12@outlook.com>
2018-07-13 13:56:51 -0400

    Psuedocoded steps to arrive to question 1

---
+++ queries.py
@@ -0,0 +1,8 @@
+1. How many total Characters are there? 302
+# Steps I took to find this answer:
+# from charactercreator.models import *
+# from armory.models import * 
+# dir(Character.objects)
+# characters = Character.objects.count()
+# characters
+
