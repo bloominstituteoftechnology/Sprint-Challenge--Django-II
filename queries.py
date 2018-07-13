62b7bdeda9368073996cd7e5e1464bec3af2df66
Austin F <austindanielfrench@gmail.com>
2018-07-13 09:49:54 -0700

    setup

---
+++ queries.py
@@ -0,0 +1,9 @@
+from charactercreator.models import Character, Fighter, Mage, Cleric, Thief, Necromancer
+from armory.models import Item, Weapon
+
+# How many total Characters are there?
+# How many of each specific subclass?
+# How many total Items?
+# How many of the Items are weapons? How many are not?
+# On average, how many Items does each Character have?
+# On average, how many Weapons does each character have?
