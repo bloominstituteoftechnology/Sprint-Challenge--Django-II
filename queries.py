06752b8b7a2c8c50b484f3b71d6c70048ef94eb2
William Pelton <williamrpelton@gmail.com>
2018-07-13 14:39:54 -0500

    got weapons answer from zoom

--- queries.py
+++ queries.py
@@ -1,26 +1,27 @@
+from django.db.models import Count, Avg
 from charactercreator.models import *
 from armory.models import *
-char_models = [Character, Fighter, Mage, Cleric, Thief, Necromancer]
+char_models = [Fighter, Mage, Cleric, Thief, Necromancer]
 
 def give_power():
     for weapon in Weapon.objects.all():
         weapon.power += 1
         weapon.save()
 def count_characters():
-    return len(Character.objects.all())
+    return Character.objects.count()
 def count_each_char():
     for model in char_models:
-        return len(model.objects.all())
+        return model.objects.count()
 def count_items():
-    return len(Item.objects.all())
+    return Item.objects.count()
 def count_weapons():
-    return len(Weapon.objects.all())
+    return Weapon.objects.count()
 def count_non_weapons():
-    return len(Item.objects.all()) - count_weapons()
+    return Item.objects.count() - count_weapons()
 def average_inventory():
     count = 0
     for character in Character.objects.all():
         count += character.inventory.count()
-    return count//Character.objets.count()
-def average_weapons:
-    pass
+    return count//Character.objects.count()
+def average_weapons():
+    return Character.objects.all().annotate(count=Count('inventory__weapon')).aggregate(Avg('count'))
