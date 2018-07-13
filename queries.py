ab9a31f8c8b279c6c8c12b6a96dae9958f62594e
charletta <charlettabullard12@outlook.com>
2018-07-13 14:28:20 -0400

    Still working on pseudocoding each question

--- queries.py
+++ queries.py
@@ -6,3 +6,23 @@
 # characters = Character.objects.count()
 # characters
 
+2. How many of each specific subclass? 
+# Steps I took to find this answer:
+# from charactercreator.models import *
+# from armory.models import *
+# dir()
+
+3. How many total Items? 174
+# Steps I took to find this answer:
+# from charactercreator.models import *
+# from armory.models import *
+# dir(Item.objects)
+# item = Item.objects.count()
+# item
+
+4. How many of the Items are weapons? How many are not?
+# Steps I took to find this answer:
+# from charactercreator.models import *
+# from armory.models import *
+# dir(Item.objects)
+
