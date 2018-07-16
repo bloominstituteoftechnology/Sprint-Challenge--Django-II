31f7e629224911da422ced0bb21d8e84e0498ae1
kiadorii <sgchoi529@email.arizona.edu>
2018-07-16 06:38:57 -0700

    Fixed Typos for question 2

--- queries.py
+++ queries.py
@@ -7,19 +7,19 @@ print('Total Characters: ', Character.objects.count())
 
 # How many of each specific subclass?
 # Mages: 108
-print('Mages: ', Mages.objects.count())
+print('Mages: ', Mage.objects.count())
 
 # Fighters: 68
-print('Fighters: ', Fighters.objects.count())
+print('Fighters: ', Fighter.objects.count())
 
 # Clerics: 75
-print('Clerics: ', Clerics.objects.count())
+print('Clerics: ', Cleric.objects.count())
 
 # Thieves: 51
-print('Thieves: ', Thieves.objects.count())
+print('Thieves: ', Thieve.objects.count())
 
 # Necromancers: 11
-print('Necromancers: ', Necromancers.objects.count())
+print('Necromancers: ', Necromancer.objects.count())
 
 # How many total Items?
 # How many of the Items are weapons? How many are not?
