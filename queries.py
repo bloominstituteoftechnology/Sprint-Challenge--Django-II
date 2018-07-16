2945d7a2351cc4b042aaa95dbc49b1155dcb356e
kiadorii <sgchoi529@email.arizona.edu>
2018-07-16 06:38:20 -0700

    Error - WIP for queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,27 @@
-- How many total Characters are there?
-- How many of each specific subclass?
-- How many total Items?
-- How many of the Items are weapons? How many are not?
-- On average, how many Items does each Character have?
-- On average, how many Weapons does each character have?
+from charactercreator.models import *
+from armory.models import *
+
+# How many total Characters are there?
+# 302
+print('Total Characters: ', Character.objects.count())
+
+# How many of each specific subclass?
+# Mages: 108
+print('Mages: ', Mages.objects.count())
+
+# Fighters: 68
+print('Fighters: ', Fighters.objects.count())
+
+# Clerics: 75
+print('Clerics: ', Clerics.objects.count())
+
+# Thieves: 51
+print('Thieves: ', Thieves.objects.count())
+
+# Necromancers: 11
+print('Necromancers: ', Necromancers.objects.count())
+
+# How many total Items?
+# How many of the Items are weapons? How many are not?
+# On average, how many Items does each Character have?
+# On average, how many Weapons does each character have?
