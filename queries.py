90986fa8b5e3b2f8e64bc1c0bdd6e33485564d12
Joshua Hall <jhall02258@gmail.com>
2020-09-24 13:23:28 -0400

    fix calculations

--- queries.py
+++ queries.py
@@ -1,20 +1,43 @@
 from django.db.models import Count, Avg
 from charactercreator.models import *
+from armory.models import Item, Weapon
 
-1. all = Character.objects.all().count()
+numberOfCharacters = Character.objects.all().count()
+fighters = Fighter.objects.count()
+necros = Necromancer.objects.count()
+thieves = Thief.objects.count()
+clerics = Cleric.objects.count()
+mages = Mage.objects.count() - necros
+weapons = Weapon.objects.count()
+charactersWithItems = Character.objects.filter(inventory__isnull=False)
 
-2. fighters = Fighter.objects.count()
-    necros = Necromancer.objects.count()
-    thieves = Thief.objects.count()
-    clerics = Cleric.objects.count()
-    mages = Mage.objects.count() - necros
+totalWeapons = 0
+for a in charactersWithItems:
+    totalWeapons += a.inventory.filter(item_id__gt=137).count()
+    print("weapon counts", totalWeapons)
 
-3. items = Item.objects.count()
+totalItems = 0
+for a in charactersWithItems:
+    totalItems += a.inventory.count()
+    print("item counts", totalItems)
 
-4. weapons = Weapon.objects.count()
-
-5. allItems = Character.objects.filter(inventory__isnull=False).count()
-
-6. allItems/all
-
-7. Character.inventory.through.objects.filter(item_id__gt=137).count()/items
+print("Total # characters", numberOfCharacters)
+# 302
+print("Total # fighters", fighters)
+# 68
+print("Total # necros", necros)
+# 11
+print("Total # thieves", thieves)
+# 51
+print("Total # clerics", clerics)
+# 75
+print("Total # items", totalItems)
+# 3286
+print("Total # weapons", totalWeapons)
+# 743
+print("Total # of non-weapon items", totalItems - totalWeapons)
+# 2543
+print("Average # of Items each character has", totalItems/numberOfCharacters)
+# 10.880794701986755
+print("Average # of Weapons each character has", totalWeapons/numberOfCharacters)
+# 2.4602649006622515
